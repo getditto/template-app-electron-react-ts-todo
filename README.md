d808d5bb4d2a2bcbea459e975b4c8a6fbf54a26d
okdistribute <633012+okdistribute@users.noreply.github.com>
2023-10-16 11:21:33 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Tasks Electron React TS
+
+This project was bootstrapped with [electron-quick-start-typescript](https://github.com/electron/electron-quick-start-typescript).
+
+1. First run `npm install`
+2. Add your Ditto AppID and token to `src/AppContainer.tsx`
+3. Run `npm start`
+
+## Node.js Integration
+
+This sample enables Electron's Node.js integration and disables context isolation to allow using Node.js APIs from the renderer process. If your renderer process loads remote content, you should isolate Ditto to the main process or to a separate renderer process to avoid introducing security vulnerabilities. Read more about this in Electron's [security docs](https://www.electronjs.org/docs/latest/tutorial/security#2-do-not-enable-nodejs-integration-for-remote-content).
+
+## Bluetooth Low Energy on macOS
+
+If the Bluetooth Low Energy P2P transport is enabled, the application might crash in development when running on macOS due to missing permissions. In order to fix this, add your terminal app (or the app that you use to run this sample) to the list of apps allowed to use Bluetooth under **System Preferences > Security & Privacy > Bluetooth**.
+
+## Bluetooth Low Energy on Linux
+
+Please refer to [the docs](https://docs.ditto.live/bluez-setup) for instructions on how to enable Bluetooth Low Energy on Linux.
---
+++ src/App.tsx
@@ -0,0 +1,82 @@
+import { useMutations, usePendingCursorOperation } from '@dittolive/react-ditto'
+import React, { useEffect, useState } from 'react'
+import './App.css'
+
+const COLLECTION = 'tasks'
+
+const App = () => {
+  const [text, setText] = useState('')
+  const { ditto, documents: tasks } = usePendingCursorOperation({
+    collection: COLLECTION,
+  })
+  const { upsert, removeByID, updateByID } = useMutations({
+    collection: COLLECTION,
+  })
+
+  useEffect(() => {
+    if (!ditto) {
+      return
+    }
+    ditto.startSync()
+
+    return () => ditto.stopSync()
+  }, [ditto])
+
+  const updateText = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setText(e.currentTarget.value)
+
+  const addTask = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    upsert({ value: { body: text, isCompleted: false } })
+    setText('')
+  }
+
+  const toggleIsCompleted =
+    (taskId: string) => (e: React.ChangeEvent<HTMLInputElement>) =>
+      updateByID({
+        _id: taskId,
+        updateClosure: (mutableDoc) => {
+          if (mutableDoc) {
+            mutableDoc.at("isCompleted").set(!mutableDoc.value.isCompleted)
+          }
+        },
+      })
+
+  const removeTask = (taskId: string) => () => removeByID({ _id: taskId })
+
+  return (
+    <div className="App">
+      <div className="count">
+        {tasks.length} task{tasks.length !== 1 ? 's' : ''}
+      </div>
+      <form className="new" onSubmit={addTask}>
+        <input
+          placeholder="New task"
+          value={text}
+          onChange={updateText}
+          required
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id.value}>
+            <input
+              type="checkbox"
+              checked={task.value.isCompleted}
+              onChange={toggleIsCompleted(task.id.value)}
+            />
+            <span className={task.value.isCompleted ? 'completed' : ''}>
+              {task.value.body} <code>(ID: {task.id.value})</code>
+            </span>
+            <button type="button" onClick={removeTask(task.id.value)}>
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
